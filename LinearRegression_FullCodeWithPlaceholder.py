bdaa0f6fc8b2122528d1535bd758b782c6201378
harrynam3@naver.com <haeul>
2019-01-04 02:42:27 +0900

    commit enter

--- LinearRegression_FullCodeWithPlaceholder.py
+++ LinearRegression_FullCodeWithPlaceholder.py
@@ -19,6 +19,5 @@ sess.run(tf.global_variables_initializer())
 for step in range(2001):
     cost_val, W_val, b_val, _ = sess.run([cost, W, b, train], feed_dict={X:[1,2,3], Y:[1,2,3]})
 
-
     if step % 20 == 0:
         print(step, cost_val, W_val, b_val)
